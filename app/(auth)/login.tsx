5480996cafb8aad255e9d7cec5c609cfd247bb68
CarnageOG <egetashviligiorgi9@gmail.com>
2026-06-17 14:49:07 +0400

    added profile page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -5,46 +5,62 @@ import { StyleSheet, View } from "react-native";
 import AppButton from "../components/appButton/AppButton";
 import AppInput from "../components/appInput/AppInput";
 
-const login = () => {
+const Login = () => {
   const router = useRouter();
+
   const [username, setUsername] = useState<string>("johnd");
   const [password, setPassword] = useState<string>("m38rmF$");
 
   const handleSubmit = async () => {
-    const response = await fetch("https://fakestoreapi.com/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+    try {
+      const response = await fetch("https://fakestoreapi.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
 
-    const result = await response.json();
-    if (result?.token) {
-      await AsyncStorage.setItem("user", JSON.stringify(result.token));
-      router.replace("/(tabs)");
+      const result = await response.json();
+
+      if (result?.token) {
+        const userResponse = await fetch("https://fakestoreapi.com/users/1");
+        const user = await userResponse.json();
+        await AsyncStorage.setItem(
+          "user",
+          JSON.stringify({ token: result.token, data: user }),
+        );
+        router.replace("/(tabs)");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
     <View style={styles.container}>
-      <AppInput placeholder={"username"} onChangeText={(t) => setUsername(t)} />
       <AppInput
-        placeholder={"password"}
-        onChangeText={(t) => setPassword(t)}
+        placeholder="username"
+        value={username}
+        onChangeText={setUsername}
+      />
+
+      <AppInput
+        placeholder="password"
+        value={password}
+        onChangeText={setPassword}
         secureTextEntry
       />
+
       <AppButton
-        title={"Log In"}
+        title="Log In"
         handlePress={handleSubmit}
         hitSlop={20}
-        disabled={username.length == 0 || password.length === 0}
+        disabled={!username || !password}
       />
     </View>
   );
 };
 
-export default login;
+export default Login;
 
 const styles = StyleSheet.create({
   container: {
