5d79cff5d9aaeeccfa9429e2f0d09984cbef2ce4
CarnageOG <egetashviligiorgi9@gmail.com>
2026-06-17 11:26:53 +0400

    123

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,62 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        animation: "shift",
+        tabBarActiveTintColor: "#FFFFFF",
+        tabBarButton: HapticTab,
+        headerStyle: {
+          backgroundColor: "#FFFFFF",
+        },
+        headerTintColor: "#000000",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Home",
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="products"
+        options={{
+          headerTitle: "Products",
+          tabBarLabel: "Products",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="bag.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: "Profile",
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="person.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          headerTitle: "Cart",
+          tabBarLabel: "Cart",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="cart.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
